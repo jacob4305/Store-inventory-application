b525da808369d9561d34ad411574eae423942852
jacob4305 <jacob4305@gmail.com>
2022-10-13 10:53:00 -0700

    update app.py

--- app.py
+++ app.py
@@ -4,24 +4,31 @@ import datetime
 import csv
 
 
-#figure out how to keep it from adding repeating items to the database!!!!!!!
 def add_csv(filename):
 	"""adds products in csv file into database"""
 	with open(filename) as csvfile:
 		inventory = csv.reader(csvfile)
 		next(csvfile)
+		#csv_product_list = []
+		#csv_name_list = []
+		db_name_list = []
 		for item in inventory:
-			#print(item)
-			product_name = item[0]
+			product = session.query(Product).filter(Product.product_name == item[0]).one_or_none()
+			csv_product_name = item[0]
 			product_price = clean_price(item[1])
 			product_quantity = clean_quantity(item[2])
 			product_date_updated = clean_date(item[3])
-			#print(product_name, product_price, product_quantity, product_date_updated)
-
-			#update_inventory = Product(product_name=product_name, product_quantity=product_quantity, product_price=product_price, date_updated=product_date_updated)
-			#session.add(update_inventory)
-			#session.commit()
-
+			update_inventory = Product(product_name=csv_product_name, product_quantity=product_quantity, product_price=product_price, date_updated=product_date_updated)
+			if product == None:	
+				session.add(update_inventory)
+			else:
+				if product.date_updated < update_inventory.date_updated:
+					session.add(update_inventory)
+					session.delete(product)
+				if product.date_updated > update_inventory.date_updated:
+					pass
+		session.commit()
+			
 
 def clean_price(price_string):
 	"""cleans string for easy use"""
@@ -65,40 +72,63 @@ def view_product():
 	"""allows the user to view aspects of a product""" 
 	while True:
 		print("Enter (menu) to go back to main menu!")
-		user_input = input("Enter product ID to view product information--> ").lower()
+		user_input = input("Enter product ID number to view product information--> ").lower()
 		if user_input == "menu":
 			break
+		if user_input.isalpha() == True:
+			print("Please enter a number ")
+			continue
 		try:
 			for item in session.query(Product):
 				user_item =session.query(Product).filter(Product.product_id==user_input).one()
 			print(f"\nProduct id: {user_item.product_id}\nProduct name: {user_item.product_name}\nProduct price: {user_item.product_price/100}\nProduct quantity: {user_item.product_quantity}\nDate updated: {user_item.date_updated}\n")
 		except sqlalchemy.exc.NoResultFound:
-			print("Product id not found")
+			print("There are no products with that id number")
 
 
 def add_new_product():
 	"""allows user to add new products to the database"""
 	while True:
-		start = input("Add new product to database? (y/n)? ")
-		if start == "n":
-			break
-		if start == "y":
-			new_name = input("Enter the new products name: ")
+		new_name = input("Enter the new products name: ")
+		try:
 			new_price = input("Enter the new products price: ")
-			new_price = clean_price(new_price)
+			new_price = clean_price('$' + str(new_price))
+		except ValueError:
+			print("Please enter a number!")
+			continue
+		try:
 			new_quantity = input("Enter the new products quantity: ")
 			new_quantity = clean_quantity(new_quantity)
-			new_date_updated = input("Enter the date updated: ")
+		except ValueError:
+			print("Please enter a number")
+			continue
+		try:
+			new_date_updated = input("Enter the date updated (month/day/four digit year): ")
 			new_date_updated = clean_date(new_date_updated)
-			add_product = input(f"Add {new_name}{new_price}{new_quantity}{new_date_updated} to the database (y/n)? ").lower()
-			if add_product == 'y':
-				new_product = Product(product_name=new_name, product_price=new_price, product_quantity=new_quantity, date_updated=new_date_updated)
-				session.add(new_product)
-				session.commit()
-			else:
-				continue
+		except (ValueError, IndexError):
+			print("Please enter a date in the format (12/22/1998): ")
+			continue
+		add_product = input(f"Add Name: {new_name} Price: {new_price/100} Quantity: {new_quantity} Date: {new_date_updated} to the database (y/n)? ").lower()
+		if add_product == 'y':
+			new_product = Product(product_name=new_name, product_price=new_price, product_quantity=new_quantity, date_updated=new_date_updated)
+			for item in session.query(Product):
+				if new_product.product_name == item.product_name:
+					if new_product.date_updated > item.date_updated:
+						session.add(new_product)
+						session.delete(item)
+						session.commit()
+						print("item added")
+					else:
+						print(f"{item.product_name}, {item.product_quantity}, {item.product_price}, updated on: {item.date_updated} is on file and has a has a more up to date!!")
+
+			
+		another_product = input("Do you want to add another product? (y/n) ").lower()
+		if another_product == 'y':
+			continue
+		if another_product == 'n':
+			break
 		else:
-			print("Please enter (y) to add new product to database or (n) exit to main menu")
+			continue
 
 
 def backup():
@@ -109,14 +139,18 @@ def backup():
 		writer.writeheader()
 		for item in session.query(Product):
 			writer.writerow({'product_name': item.product_name, 'product_price': '$'+(str(item.product_price/100)), 'product_quantity': item.product_quantity, 'date_updated': item.date_updated.strftime('%-m/%-d/%Y')})
+		print("File has been backed up!!!")
 
 
 if __name__ == "__main__":
 	Base.metadata.create_all(engine)
-	add_csv('inventory.csv')
-	menu()
-	#clean_csv_price('$7.41')
-	#clean_csv_date('3/10/2018')
+	#o = session.query(Product).filter(Product.product_id=='26').one()
+	#session.delete(o)
+	#session.commit()
+	
 	#for i in session.query(Product):
 		#session.delete(i)
 		#session.commit()
+	add_csv('inventory.csv')
+	
+	menu()
